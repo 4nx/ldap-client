cc188174adee20de95a5a7abd565176fa009ce28
Simon Krenz <simon.krenz@tuta.io>
2020-05-25 23:30:53 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module ldap-client
+
+go 1.14
+
+require gopkg.in/ldap.v3 v3.1.0
---
+++ ldap-client.go
@@ -0,0 +1,93 @@
+package ldap
+
+import (
+	"crypto/tls"
+	"fmt"
+	"log"
+
+	"gopkg.in/ldap.v3"
+)
+
+// LdapConfig will hold the config which are needed to connect and search
+type LdapConfig struct {
+	Conn         *ldap.Conn
+	Host         string
+	Port         int
+	BindUser     string
+	BindPassword string
+	BaseDN       string
+	ServerName   string
+	UserSearch   string // e.g. (objectclass=user)(sAMAccount=%s)
+}
+
+func (lc *LdapConfig) Close() {
+	if lc.Conn != nil {
+		lc.Conn.Close()
+		lc.Conn = nil
+	}
+}
+
+func (lc *LdapConfig) ldapsConnect() error {
+	if lc.Conn == nil {
+		var l *ldap.Conn
+		serverAddress := fmt.Sprintf("%s:%d", lc.Host, lc.Port)
+
+		tlsConfig := tls.Config{
+			// TODO: add root ca to this
+			InsecureSkipVerify: true,
+			ServerName:         lc.ServerName,
+			CipherSuites: []uint16{
+				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			},
+			MinVersion: tls.VersionTLS12,
+		}
+
+		l, err := ldap.DialTLS("tcp", serverAddress, &tlsConfig)
+		if err != nil {
+			return err
+		}
+		lc.Conn = l
+	}
+	return nil
+}
+
+func (lc *LdapConfig) SearchUser(username string) (string, error) {
+	err := lc.ldapsConnect()
+	if err != nil {
+		return "", err
+	}
+
+	err = lc.Conn.Bind(lc.BindUser, lc.BindPassword)
+	if err != nil {
+		log.Printf("Failed to bind: %v", err)
+		return "", err
+	}
+
+	searchRequest := ldap.NewSearchRequest(
+		lc.BaseDN,
+		ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, 0, false,
+		fmt.Sprintf("(&"+lc.UserSearch+")", username),
+		[]string{"dn"},
+		nil,
+	)
+
+	s, err := lc.Conn.Search(searchRequest)
+	if err != nil {
+		log.Printf("User search failed: %v", err)
+		return "", err
+	}
+
+	if len(s.Entries) != 1 {
+		if len(s.Entries) == 0 {
+			log.Printf("User not found: %s", username)
+		} else {
+			log.Printf("Too many results: %d", len(s.Entries))
+		}
+	}
+
+	userdn := s.Entries[0].DN
+	log.Printf("User DN found: %s", userdn)
+	return userdn, nil
+}
