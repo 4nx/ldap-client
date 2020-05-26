8559e2d356655cab5c5c35c8099fd1086c6d1638
Simon Krenz <simon.krenz@tuta.io>
2020-05-26 23:36:56 +0200

    temp disable cipher suites

--- ldap-client.go
+++ ldap-client.go
@@ -36,11 +36,12 @@ func (lc *LdapConfig) ldapsConnect() error {
 			// TODO: add root ca to this
 			InsecureSkipVerify: true,
 			ServerName:         lc.ServerName,
-			CipherSuites: []uint16{
-				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-			},
+			// TODO: set CipherSuites
+			//CipherSuites: []uint16{
+			//	tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			//	tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			//	tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			//},
 			MinVersion: tls.VersionTLS12,
 		}
 
